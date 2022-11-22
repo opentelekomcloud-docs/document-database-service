155951cb65c687ee81cac2addc0d5ee10c96d83c
Artem Goncharov <artem.goncharov@gmail.com>
2022-11-22 13:44:59 +0100

    Update tox.ini && conf.py file
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py

--- umn/source/conf.py
+++ umn/source/conf.py
@@ -28,8 +28,6 @@ project = 'Document Database Service'
 otcdocs_repo_name = 'opentelekomcloud-docs/document-database-service'
 # Those variables are required for edit/bug links
 
-
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -93,7 +91,7 @@ html_static_path = ['_static']
 
 # -- Options for PDF output --------------------------------------------------
 latex_documents = [
-('index',
+    ('index',
      'dds-umn.tex',
      u'Document Database Service - User Guide',
      u'OpenTelekomCloud', 'manual'),
