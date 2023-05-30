b73d6ab27733a08bec2b5ab8ca65596369ecb294
tischrei <tino.schreiber@t-systems.com>
2023-05-30 10:04:02 +0000

    Update tox.ini && conf.py file
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py

--- api-ref/source/conf.py
+++ api-ref/source/conf.py
@@ -28,6 +28,15 @@ project = 'Document Database Service'
 otcdocs_repo_name = 'opentelekomcloud-docs/document-database-service'
 # Those variables are required for edit/bug links
 
+# Those variables are needed for indexing into OpenSearch
+otcdocs_doc_environment = 'public'
+otcdocs_doc_link = '/document-database-service/api-ref/'
+otcdocs_doc_title = 'API Reference'
+otcdocs_doc_type = 'api-ref'
+otcdocs_service_category = 'database'
+otcdocs_service_title = 'Document Database Service'
+otcdocs_service_type = 'dds'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
